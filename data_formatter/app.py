c3b1649687a3113b4cb5a89c3ec675a4387dda8f
Sebastiano Brischetto <sebybrischetto@gmail.com>
2023-12-21 17:58:47 +0100

    feat(controllo dati registrazione): microservizio che esegue il controllo dei dati di registrazione

--- data_formatter/app.py
+++ data_formatter/app.py
@@ -9,25 +9,20 @@ endpoint_cities_db = "http://127.0.0.1:5000/"
 #formatta i dati ricevuti prima di inserirli nel db
 @app.route('/format_data', methods=['POST'])
 def format_data():
-    unfiltered_data = request.get_json()
-    if not unfiltered_data:
+    data = request.get_json()
+    if not data:
         return abort(400)
-    filtered_data = {"weather_data": unfiltered_data.pop("list")[:8], "city": unfiltered_data.pop("city")}
-    response = requests.get(endpoint_cities_db+"cities",{"city" : filtered_data["city"]["name"]})
-    if response.status_code == 200:
-        update = True
-    else:
-        update = False
-    save_to_db(filtered_data, update)
+    formatted_data = {"weather_data": data.pop("list")[:8], "city": data.pop("city")}
+    save_to_db(formatted_data)
     return jsonify({"message": "richiesta effettuata con successo"})
 
 def save_to_db(data, update):
     headers = {'Content-Type': 'application/json'}
-    if update:
+    response = requests.get(endpoint_cities_db+"cities",{"city" : data["city"]["name"]})
+    if response.status_code == 200:
         requests.post(endpoint_cities_db + "update_weather_data", headers=headers, json=data)
     else:
         requests.post(endpoint_cities_db + "save_weather_data", headers=headers, json=data)
-    return
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ register_form/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, abort
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+types = ["temperatura", "umidità", "pressione", "vento"]
+conditions = ["<",">"]
+
+#controlla i parametri prima di accettare la registrazione
+#Parametri:
+# @city - nome citta
+# @type - tipo condizione da controllare
+# @condition - condizione 
+# @value - valore 
+
+@app.route('/check_form' , methods=['GET'])
+def check_form():
+    city = request.args.get('city')
+    type = request.args.get('type')
+    condition = request.args.get('condition')
+    value = request.args.get('value')
+    checks = [check_city(city), check_type(type), check_condition(condition), check_value(value)]
+    if all(checks):
+        return jsonify({"message": "richiesta effettuata con successo"})
+    return abort(400)
+
+def check_city(city):
+    if city and city!="":
+        return True
+    return False
+
+def check_type(type):
+    if type in types:
+        return True
+    return False
+
+def check_condition(condition):
+    if condition in conditions:
+        return True
+    return False
+
+def check_value(value):
+    try:
+        value = float(value)
+    except:
+        return False
+    return True
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
