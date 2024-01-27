9a5bd69912b298dd6d75dc70b9fe13b177b0cfc0
Sebastiano Brischetto <sebybrischetto@gmail.com>
2024-01-27 19:08:08 +0100

    feat(deploy e sla manager): deploy, prometheus e sla manager

--- uservice_client/telegram_bot/main.py
+++ uservice_client/telegram_bot/main.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == '__main__':
     weather_bot = WeatherBot(
-        "6765515091:AAGSMzDzfw4f5zrrZ3FF8Lzboz5g2uUY9ZE", 
+        os.environ.get('BOT_TOKEN'),
         os.environ.get('REGISTER_USER'), 
         os.environ.get('REMOVE_USER'))
     weather_bot.run()
